408fab74d1d63b0139332af6021314249acf0668
Jaret <gadir041499@gmail.com>
2024-02-27 20:49:10 -0600

    Fix formatting issues in app.php and database.php

--- includes/app.php
+++ includes/app.php
@@ -1,7 +1,7 @@
 <?php 
 use Model\ActiveRecord;
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ );
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';
--- includes/database.php
+++ includes/database.php
@@ -4,10 +4,10 @@ $db = mysqli_connect(
     $_ENV['DB_HOST'],
     $_ENV['DB_USER'],
     $_ENV['DB_PASSWORD'],
-    $_ENV['DB_NAME']
+    $_ENV['DB_NAME'],
 );
 
-$db->set_charset("utf8");
+$db->set_charset('utf8');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
